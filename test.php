5a2362731ea2b37dff6d664bdad7fbcc8369a275
dr_Lev <drlev52@gmail.com>
2021-07-09 16:45:50 +0300

    2

--- test.php
+++ test.php
@@ -18,3 +18,12 @@ assert(even_to_zero(12345) == 10305);
 assert(even_to_zero(13572) == 10502);
 assert(even_to_zero(2222) == 2020);
 assert(even_to_zero(12300321) == 10300020);
+
+function is_palindrom(string $word)
+{
+    return $word == implode('', array_reverse(mb_str_split($word)));
+}
+
+assert(is_palindrom('шалаш'));
+assert(is_palindrom('казак'));
+assert(is_palindrom('это другое'));
