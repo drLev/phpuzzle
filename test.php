68b241c7c718ed96f95dcfebe2bbf74b0a02a305
dr_Lev <drlev52@gmail.com>
2021-07-09 16:39:08 +0300

    1

--- test.php
+++ test.php
@@ -1,4 +1,20 @@
 <?php
 
-assert(true);
-assert(false);
+function even_to_zero(int $value)
+{
+    return (int) implode(
+        '',
+        array_map(
+            function($item) {
+                static $index = 0;
+                $index++;
+                return $index%2?$item:0;
+            },
+            str_split($value)
+        )
+    );
+}
+assert(even_to_zero(12345) == 10305);
+assert(even_to_zero(13572) == 10502);
+assert(even_to_zero(2222) == 2020);
+assert(even_to_zero(12300321) == 10300020);
